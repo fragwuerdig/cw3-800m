a279b84b165a7abcc562e09a37805cb20dcdd498
Till Ziegler <tz@schoeneweide.tk>
2024-02-15 23:43:53 +0100

    add verify script

---
+++ verify.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+# This script is used to verify the SHA256 hash of the on-chain wasm
+# file against the build artifact from the code of this repository 
+# Assuming following installations:
+# - jq 
+# - shasum
+# - curl
+# - base64
+# - docker
+
+set -e
+
+# TODO: Replace with the correct code id
+curl -sSX GET "https://terra-classic-lcd.publicnode.com:443/cosmwasm/wasm/v1/code/917" -H  "accept: application/json" | jq -r '.data' > download_base64.wasm
+base64 -d download_base64.wasm > download_raw.wasm
+
+./build.sh
+SHA_DOWNLOAD=$(shasum -a 256 download_raw.wasm | awk '{print $1}')
+SHA_LOCAL=$(shasum -a 256 artifacts/cw3_fixed_multisig.wasm | awk '{print $1}')
+
+echo "Downloaded SHA:     $SHA_DOWNLOAD"
+echo "Local Built SHA:    $SHA_LOCAL"
+
